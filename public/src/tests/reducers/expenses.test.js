bfc3e0018c0f3ee5b56723ed9659e81da468fc95
Greta <50132963+GretaVait@users.noreply.github.com>
2020-11-02 14:28:31 +0200

    testing filters reducers

---
+++ public/src/tests/reducers/expenses.test.js
@@ -0,0 +1,72 @@
+import expensesReducer from '../../reducers/expenses';
+import expenses from '../fixtures/expenses';
+
+test('should set default state', () => {
+  const state = expensesReducer(undefined, { type: '@@INIT' });
+
+  expect(state).toEqual([]);
+});
+
+test('should remove expense by id', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: expenses[1].id
+  }
+  const state = expensesReducer(expenses, action);
+
+  expect(state).toEqual([ expenses[0], expenses[2] ]);
+})
+
+test('should  not remove expense if id not found', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: '-1'
+  }
+  const state = expensesReducer(expenses, action);
+
+  expect(state).toEqual(expenses);
+})
+
+test('should add an expense', () => {
+  const expenseData = {
+    description: 'Rent', 
+    amount: 109500,
+    createdAt: 1000,
+    note: 'This was last months rent'
+  };
+
+  const action = {
+    type: 'ADD_EXPENSE',
+    expense: expenseData
+  }
+
+  const state = expensesReducer(expenses, action);
+
+  expect(state).toEqual([ ...expenses, expenseData ]);
+})
+
+test('should edit expense by id', () => {
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: expenses[1].id,
+    updates: {
+      description: 'New Description'
+    }
+  }
+  const state = expensesReducer(expenses, action);
+  expect(state[1].description).toBe( 'New Description' );
+})
+
+test('should not edit expense if id not found', () => {
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: '-1',
+    updates: {
+      description: 'New Description'
+    }
+  }
+  const state = expensesReducer(expenses, action);
+
+  expect(state).toEqual(expenses);
+})
+
--- public/src/tests/selectors/expenses.test.js
+++ public/src/tests/selectors/expenses.test.js
@@ -1,29 +1,6 @@
 import selectExpenses from '../../selectors/expenses';
 import moment from 'moment';
-
-const expenses = [
-  {
-    id: '1',
-    description: 'Gas',
-    note: '',
-    amount: 195,
-    createdAt: 0
-  },
-  {
-    id: '2',
-    description: 'Rent',
-    note: '',
-    amount: 109600,
-    createdAt: moment(0).subtract(4, 'days').valueOf()
-  },
-  {
-    id: '3',
-    description: 'Candy',
-    note: '',
-    amount: 4500,
-    createdAt: moment(0).add(4, 'days').valueOf()
-  }
-]
+import expenses from '../fixtures/expenses';
 
 test('should filter by text value', () => {
   const filters = {
