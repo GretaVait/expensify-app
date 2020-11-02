f87d81035e2bf2983b554d7551fc1c7cba99c836
Greta <50132963+GretaVait@users.noreply.github.com>
2020-11-02 14:50:42 +0200

    production webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,32 +3,36 @@
 const path = require('path');
 const { forwardRef } = require('react');
 
-module.exports = {
-  entry: './public/src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  //loader
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
+module.exports = (env) => {
+  const isProd = env === 'production';
+
+  return {
+    entry: './public/src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
     },
-    {
-      test: /\.sass$|\.css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
+    //loader
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.sass$|\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
       ]
+    },
+    devtool: isProd ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
     }
-    ]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
   }
 };
