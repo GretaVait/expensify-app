866f4a2b557111de0f33e58870137a8c2b9c7ff8
Greta <50132963+GretaVait@users.noreply.github.com>
2020-11-03 23:33:27 +0200

    adding summary component

---
+++ public/src/components/ExpensesSummary.js
@@ -0,0 +1,24 @@
+import React from "react";
+import  { connect } from 'react-redux';
+import selectExpenseTotal from '../selectors/expenses-total';
+import numeral from 'numeral';
+import selectExpenses from '../selectors/expenses';
+
+const ExpenseSummary = ( {expenseCount, expenseTotal} ) => {
+  const expenseWord = expenseCount == 1 ? 'expense' : 'expenses';
+  return (
+    <div>
+      <p>Viewing {expenseCount} {expenseWord} in total {numeral(expenseTotal / 100).format('$0,0.00')}</p>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  const visibleExpenses = selectExpenses(state.expenses, state.filters);
+  return {
+    expenseCount: visibleExpenses.length,
+    expenseTotal: selectExpenseTotal(visibleExpenses)
+  };
+}
+
+export default connect(mapStateToProps)(ExpenseSummary);
