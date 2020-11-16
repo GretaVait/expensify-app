d2ecb63cde8881fbba0ff99de8bb8d9d5cb48c64
Greta <50132963+GretaVait@users.noreply.github.com>
2020-11-16 11:47:42 +0200

    creating test database

--- public/src/firebase/firebase.js
+++ public/src/firebase/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 
 // Web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBxrpjE3GizVfRcve8fREkASEsFo3BagI4",
-  authDomain: "expensify-673f9.firebaseapp.com",
-  databaseURL: "https://expensify-673f9.firebaseio.com",
-  projectId: "expensify-673f9",
-  storageBucket: "expensify-673f9.appspot.com",
-  messagingSenderId: "642990906427",
-  appId: "1:642990906427:web:b5767d91f45df6c65c501e"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
